e377e700c1da604fd11d0776016befe018a14ae8
HyunJoong Kim <hjk0761@naver.com>
2024-02-28 20:01:42 +0900

    feat: 방명록 기능 추가

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const name = document.getElementById("name");
+
+const checkbox1 = document.getElementById("movie1");
+const checkbox2 = document.getElementById("movie2");
+const checkbox3 = document.getElementById("movie3");
+const checkbox4 = document.getElementById("movie4");
+const checkbox5 = document.getElementById("movie5");
+
+const button = document.getElementById("register");
+
+function countMovie(){
+    let count = 0;
+    if (checkbox1.checked === true) {
+        count = count + 1;
+    }
+    if (checkbox2.checked === true) {
+        count = count + 1;
+    }
+    if (checkbox3.checked === true) {
+        count = count + 1;
+    }
+    if (checkbox4.checked === true) {
+        count = count + 1;
+    }
+    if (checkbox5.checked === true) {
+        count = count + 1;
+    }
+    return count;
+}
+
+function favorite(){
+    let count = countMovie();
+    alert(name.value + "님, 저와 " + count + "개의 취향이 같으시네요!");
+}
+
+button.addEventListener("click", favorite);
