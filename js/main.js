966a3a9a4eb7e3aefceedc103218fed2a4040071
HyunJoong Kim <hjk0761@naver.com>
2024-02-28 21:07:08 +0900

    refactor: 컴포넌트 순회 로직 변경

--- js/main.js
+++ js/main.js
@@ -1,29 +1,21 @@
 const name = document.getElementById("name");
 
-const checkbox1 = document.getElementById("movie1");
-const checkbox2 = document.getElementById("movie2");
-const checkbox3 = document.getElementById("movie3");
-const checkbox4 = document.getElementById("movie4");
-const checkbox5 = document.getElementById("movie5");
+const movielist = [
+    document.getElementById("movie1"),
+    document.getElementById("movie2"),
+    document.getElementById("movie3"),
+    document.getElementById("movie4"),
+    document.getElementById("movie5")
+];
 
 const button = document.getElementById("register");
 
 function countMovie(){
     let count = 0;
-    if (checkbox1.checked === true) {
-        count++;
-    }
-    if (checkbox2.checked === true) {
-        count++;
-    }
-    if (checkbox3.checked === true) {
-        count++;
-    }
-    if (checkbox4.checked === true) {
-        count++;
-    }
-    if (checkbox5.checked === true) {
-        count++;
+    for (let i = 0; i < 5; i++){
+        if (movielist[i].checked === true) {
+            count++;
+        }
     }
     return count;
 }
